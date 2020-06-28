35d96ad72a12af3365ca1a8332c4ae935e8df1ce
Saurav <saurav@childhealthimprints.com>
2020-06-28 19:08:59 +0530

    minor change

--- less_than_1500.py
+++ less_than_1500.py
@@ -30,46 +30,6 @@ import psycopg2
 from random import shuffle
 
 
-
-con = psycopg2.connect (user = 'postgres',
-                password = 'postgres',
-                port = '5432',
-                host = 'localhost',
-                database = 'inicudb')
-
-cur  = con.cursor()
-
-print("second")
-
-cur10 = con.cursor()
-cur10.execute("SELECT DISTINCT(uhid) FROM apollo.baby_detail WHERE dateofadmission >= '2018-07-01' AND dateofadmission <= '2020-05-31' and UHID IN  (select distinct(uhid) from apollo.babyfeed_detail where uhid in  ( select distinct(uhid) from apollo.baby_visit where uhid in (select  distinct(uhid) from apollo.nursing_vitalparameters where uhid in ( select distinct(uhid) from apollo.device_monitor_detail UNION select distinct(uhid) from apollo.device_monitor_detail_dump ))))and (dischargestatus = 'Death'  OR dischargestatus = 'Discharge') and isreadmitted is not true and gestationweekbylmp is not null and birthweight is not null;")
-cols10 = list(map(lambda x: x[0], cur10.description))
-fixed = pd.DataFrame(cur10.fetchall(),columns=cols10)
-
-#defining the early stopping criteria
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,restore_best_weights=True,patience=3)
-
-
-# In[ ]:
-
-print("2222")
-gq = pd.read_csv('data_23_june.csv')
-#gq.drop('Unnamed: 0',axis=1,inplace=True)
-t = gq.groupby('uhid').count()
-t.reset_index(inplace=True)
-t = t[['uhid','Unnamed: 0']]
-gq.drop('Unnamed: 0',axis=1,inplace=True)
-gs = pd.merge(gq,t,on=['uhid'],how='left')
-
-#gs.drop('Unnamed: 0.1',axis=1,inplace=True)
-print("111")
-ids = gs.uhid.unique()
-
-print(gs.columns)
-
-# In[ ]:
-
-
 #70:30 split
 def split_70(x):
     return int((round((x/15)*0.7))*15)
@@ -84,30 +44,38 @@ def mean_confidence_interval(data, confidence=0.95):
 
 
 # Function to select random patients
-
+# For given set of death patients, select similar profile discharge patients
 def randomize(dis,dea):
     #Unnamed: 0 is used as a proxy for LOS in minutes
     df = pd.DataFrame(columns=dis.columns)
+    #the gestation categories are <26, 26-28, 28-32, and > 32
+    #find out death and discharge case for gestation less than 26 weeks
     dis_less_26 = dis[dis['gestation']<=26]
     dea_less_26 = dea[dea['gestation']<=26]
     dis_less_26.sort_values(['Unnamed: 0','uhid'],ascending=False)
     dea_less_26.sort_values(['Unnamed: 0','uhid'],ascending=False)
+    #find unirque patient ids for selected gestation
     dis_less_26_uhid = dis_less_26.uhid.unique()
     dea_less_26_uhid = dea_less_26.uhid.unique()
     taken = []
     tk = set()
+    #iterated over all death cases meeting the gestation category condition
     for i in dea_less_26.uhid.unique():
         x_dea = dea_less_26[dea_less_26['uhid']==i]
-        l = x_dea['Unnamed: 0'].iloc[0]
-        x_dis = dis_less_26[dis_less_26['Unnamed: 0']>=l]
+        #Unnamed: 0 denotes number of rows per minute for given UHID
+        minutesDataDeathHDPPatient = x_dea['Unnamed: 0'].iloc[0]
+        #Find all discharge cases that meet the condition > = Unnamed: 0 
+        x_dis = dis_less_26[dis_less_26['Unnamed: 0']>=minutesDataDeathHDPPatient]
+        #remove the selected UHID from the available selection candidates
         ids_dis = set(x_dis.uhid.unique())
         ids = list(ids_dis - ids_dis.intersection(tk))
-
         shuffle(ids)
         x = dis_less_26[dis_less_26['uhid']==ids[0]]
+        #pick the data from discharge patient for same number of minutes as in death case
         x = x[:len(x_dea)]
         y = pd.concat([x,x_dea])
         taken.append(ids[0])
+        #remove the selected UHID from the available selection candidates by inserting it in tk        
         tk = set(taken)
         df = df.append(y,ignore_index=True)
     taken = []
@@ -174,21 +142,6 @@ def randomize(dis,dea):
         tk = set(taken)
         df = df.append(y,ignore_index=True)
 
-
-
-
-
-
-
-
-    #df = pd.DataFrame(columns=dt.columns)
-
-    #shuffle(ids)
-    #uhid = ids[:50]
-    #for i in uhid:
-    #    x = dt[dt['uhid']==i]
-    #    df = df.append(x,ignore_index=True)
-
     return df
 
 #Function for converting data to multiples of 15
@@ -196,12 +149,6 @@ def range_finder(x):
     length = x
     fractional = (x/15.0) - math.floor(x/15.0)
     return int(round(fractional*15))
-
-
-cont = ['pulserate',
-       'ecg_resprate', 'spo2', 'heartrate', 'dischargestatus', 'uhid']
-
-#gd = gs[cont]
 
 def make_lstm(gs):
     #gs1 = gs[gs['birthweight']<=1500]
@@ -354,208 +301,164 @@ def lstm_model(n,gd):
         except:
             continue
     return auc_roc_inter,list(itertools.chain(*val_a)),list(itertools.chain(*train_a))
-f_a = []
-i_a = []
-c_a = []
-ci_a = []
-cf_a = []
-fi_a = []
-a = []
+
+try:
+    con = psycopg2.connect (user = 'postgres',
+                    password = 'postgres',
+                    port = '5432',
+                    host = 'localhost',
+                    database = 'inicudb')
+
+    cur  = con.cursor()
+
+    print("connected to database")
+
+    cur10 = con.cursor()
+    cur10.execute("SELECT DISTINCT(uhid) FROM apollo.baby_detail WHERE dateofadmission >= '2018-07-01' AND dateofadmission <= '2020-05-31' and UHID IN  (select distinct(uhid) from apollo.babyfeed_detail where uhid in  ( select distinct(uhid) from apollo.baby_visit where uhid in (select  distinct(uhid) from apollo.nursing_vitalparameters where uhid in ( select distinct(uhid) from apollo.device_monitor_detail UNION select distinct(uhid) from apollo.device_monitor_detail_dump ))))and (dischargestatus = 'Death'  OR dischargestatus = 'Discharge') and isreadmitted is not true and gestationweekbylmp is not null and birthweight is not null;")
+    cols10 = list(map(lambda x: x[0], cur10.description))
+    fixed = pd.DataFrame(cur10.fetchall(),columns=cols10)
+
+    #defining the early stopping criteria
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,restore_best_weights=True,patience=3)
+
+    print("before reading CSV file")
+    gq = pd.read_csv('data_23_june.csv')
+    #gq.drop('Unnamed: 0',axis=1,inplace=True)
+    t = gq.groupby('uhid').count()
+    t.reset_index(inplace=True)
+    t = t[['uhid','Unnamed: 0']]
+    gq.drop('Unnamed: 0',axis=1,inplace=True)
+    gs = pd.merge(gq,t,on=['uhid'],how='left')
+
+    #gs.drop('Unnamed: 0.1',axis=1,inplace=True)
+    print("CSV file loaded")
+    ids = gs.uhid.unique()
+
+    print(gs.columns)
 
 
-for i in range(5):
-
-    gs1 = gs[gs['birthweight']<=1500]
-    gs2 = gs[(gs['birthweight']>2500)&(gs['birthweight']<3000)]
-
-    #balancing the dataset
-    death1 = gs1[gs1['dischargestatus']==1]
-    death_l1 = death1.uhid.unique()
-    death2 = gs2[gs2['dischargestatus']==1]
-    death_l2 = death2.uhid.unique()
-    dis1 = gs1[gs1['dischargestatus']==0]
-    dis_l1 = dis1.uhid.unique()
-    dis2 = gs2[gs2['dischargestatus']==0]
-    dis_l2 = dis2.uhid.unique()
-    print(len(dis1))
-    final = pd.DataFrame()
-    final_df = pd.DataFrame(columns=gs.columns)
-    d1 = randomize(dis1,death1)
-    d2 = randomize(dis2,death2)
-    gw = pd.concat([d1,d2])
-
-
-
-    fixed = ['dischargestatus',  'gender', 'birthweight',
-           'birthlength', 'birthheadcircumference', 'inout_patient_status',
-           'gestationweekbylmp', 'gestationdaysbylmp',
-           'baby_type', 'central_temp', 'apgar_onemin', 'apgar_fivemin',
-           'apgar_tenmin', 'motherage', 'conception_type', 'mode_of_delivery',
-           'steroidname', 'numberofdose', 'gestation','uhid']
-
-
-    # In[ ]:
-
-    gd = gw[fixed]
-
-
-
-    # In[ ]:
-
-
-
-    an = lstm_model(18,gd)
-
-    f_a.append(an[0])
-
-
-    #f_b = mean_confidence_interval(an[1])
-
-
-
-    #f_c = mean_confidence_interval(an[2])
-
-    #print('Fixed')
-    #print(f_a,f_b,f_c)
-    #print(an[0])
-
-
-
-
-    inter = ['dischargestatus', 'mean_bp',
-           'sys_bp', 'dia_bp', 'peep', 'pip', 'map', 'tidalvol',
-           'minvol', 'ti', 'fio2',
-           'abd_difference_y',
-           'abdomen_girth_y', 'currentdateheight', 'currentdateweight',
-
-           'new_ph', 'rbs',
-           'stool_day_total', 'temp',
-           'total_intake', 'totalparenteralvolume',
-           'tpn-tfl', 'typevalue_Antibiotics', 'typevalue_Inotropes',
-           'urine', 'urine_per_hour',
-           'urine_per_kg_hour', 'uhid']
-
-
-    # In[ ]:
-
-
-    gd = gw[inter]
-
-
-    an = lstm_model(26,gd)
-
-    i_a.append(an[0])
-    #i_b = mean_confidence_interval(an[1])
-    #i_c = mean_confidence_interval(an[2])
-
-    #print('Inter')
-    #print(i_a,i_b,i_c)
-    #print(an[0])
     cont = ['pulserate',
            'ecg_resprate', 'spo2', 'heartrate', 'dischargestatus', 'uhid']
 
-
-    gd = gw[cont]
-
-    an = lstm_model(4,gd)
-
-    c_a.append(an[0])
+    #gd = gs[cont]
 
 
-    #c_b = mean_confidence_interval(an[1])
+    f_a = []
+    i_a = []
+    c_a = []
+    ci_a = []
+    cf_a = []
+    fi_a = []
+    a = []
+
+    print("Run iterations for different discharge and death combinations")
+    for i in range(5):
+
+        gs1 = gs[gs['birthweight']<=1500]
+        gs2 = gs[(gs['birthweight']>2500)&(gs['birthweight']<3000)]
+
+        #balancing the dataset
+        death1 = gs1[gs1['dischargestatus']==1]
+        death_l1 = death1.uhid.unique()
+        death2 = gs2[gs2['dischargestatus']==1]
+        death_l2 = death2.uhid.unique()
+        dis1 = gs1[gs1['dischargestatus']==0]
+        dis_l1 = dis1.uhid.unique()
+        dis2 = gs2[gs2['dischargestatus']==0]
+        dis_l2 = dis2.uhid.unique()
+        print(len(dis1))
+        final = pd.DataFrame()
+        final_df = pd.DataFrame(columns=gs.columns)
+        d1 = randomize(dis1,death1)
+        d2 = randomize(dis2,death2)
+        gw = pd.concat([d1,d2])
 
 
+        #---------------FIXED------------------
+        fixed = ['dischargestatus',  'gender', 'birthweight',
+               'birthlength', 'birthheadcircumference', 'inout_patient_status',
+               'gestationweekbylmp', 'gestationdaysbylmp',
+               'baby_type', 'central_temp', 'apgar_onemin', 'apgar_fivemin',
+               'apgar_tenmin', 'motherage', 'conception_type', 'mode_of_delivery',
+               'steroidname', 'numberofdose', 'gestation','uhid']
+        gd = gw[fixed]
+        an = lstm_model(18,gd)
+        f_a.append(an[0])
+        print("fixed")
+        print(mean_confidence_interval(an[0]))
+        #---------------INTERMITTENT------------------
+        inter = ['dischargestatus', 'mean_bp',
+               'sys_bp', 'dia_bp', 'peep', 'pip', 'map', 'tidalvol',
+               'minvol', 'ti', 'fio2',
+               'abd_difference_y',
+               'abdomen_girth_y', 'currentdateheight', 'currentdateweight',
 
-    #c_c = mean_confidence_interval(an[2])
+               'new_ph', 'rbs',
+               'stool_day_total', 'temp',
+               'total_intake', 'totalparenteralvolume',
+               'tpn-tfl', 'typevalue_Antibiotics', 'typevalue_Inotropes',
+               'urine', 'urine_per_hour',
+               'urine_per_kg_hour', 'uhid']
 
-    #print('Cont')
-    #print(c_a,c_b,c_c)
-    #print(an[0])
-    cont_inter = list(set(cont+inter))
-    gd = gw[cont_inter]
+        gd = gw[inter]
+        an = lstm_model(26,gd)
+        i_a.append(an[0])
+        print("inter")
+        print(mean_confidence_interval(an[0]))      
+        #---------------CONTINUOUS------------------
+        cont = ['pulserate',
+               'ecg_resprate', 'spo2', 'heartrate', 'dischargestatus', 'uhid']
+        gd = gw[cont]
+        an = lstm_model(4,gd)
+        c_a.append(an[0])
+        print("cont")
+        print(mean_confidence_interval(an[0]))      
+        #---------------CONT+INTER------------------
+        cont_inter = list(set(cont+inter))
+        gd = gw[cont_inter]
+        an = lstm_model(30,gd)
+        ci_a.append(an[0])
+        print("cont_inter")
+        print(mean_confidence_interval(an[0]))              
+        #---------------FIXED+INTER------------------
+        fixed_inter = list(set(fixed+inter))
+        gd = gw[fixed_inter]
+        an = lstm_model(44,gd)
+        fi_a.append(an[0])
+        print("fixed_inter")
+        print(mean_confidence_interval(an[0]))              
+        #---------------CONT+FIXED------------------
+        cont_fixed = list(set(cont+fixed))
+        gd = gw[cont_fixed]
+        an = lstm_model(22,gd)
+        cf_a.append(an[0])
+        print("cont_fixed")
+        print(mean_confidence_interval(an[0]))                      
+        #---------------CONT+FIXED+INTER------------------
+        all_cols = list(set(cont+inter+fixed))
+        gd = gw[all_cols]
+        an = lstm_model(48,gd)
+        a.append(an[0])
+        print("all_cols")
+        print(mean_confidence_interval(an[0]))                      
 
-    an = lstm_model(30,gd)
+    print('Fixed')
+    print(mean_confidence_interval(list(itertools.chain(*f_a))))
+    print('Inter')
+    print(mean_confidence_interval(list(itertools.chain(*i_a))))
+    print('Cont')
+    print(mean_confidence_interval(list(itertools.chain(*c_a))))
+    print('Cont+inter')
+    print(mean_confidence_interval(list(itertools.chain(*ci_a))))
+    print('Fixed+Inter')
+    print(mean_confidence_interval(list(itertools.chain(*fi_a))))
+    print('Cont+Fixed')
+    print(mean_confidence_interval(list(itertools.chain(*cf_a))))
+    print('All')
+    print(mean_confidence_interval(list(itertools.chain(*a))))
 
-    ci_a.append(an[0])
-
-
-    #ci_b = mean_confidence_interval(an[1])
-
-
-
-    #ci_c = mean_confidence_interval(an[2])
-
-    #print('Cont_Inter')
-    #print(ci_a,ci_b,ci_c)
-    #print(an[0])
-
-
-    fixed_inter = list(set(fixed+inter))
-
-
-    gd = gw[fixed_inter]
-    an = lstm_model(44,gd)
-
-    fi_a.append(an[0])
-
-
-    #fi_b = mean_confidence_interval(an[1])
-
-
-
-    #fi_c = mean_confidence_interval(an[2])
-
-    #print('Fixed_Inter')
-    #print(fi_a,fi_b,fi_c)
-    #print(an[0])
-
-    cont_fixed = list(set(cont+fixed))
-    gd = patients[cont_fixed]
-    an = lstm_model(22,gd)
-
-    cf_a.append(an[0])
-
-
-    #cf_b = mean_confidence_interval(an[1])
-
-
-
-    #cf_c = mean_confidence_interval(an[2])
-
-    #print('Fixed_Cont')
-    #print(cf_a,cf_b,cf_c)
-    #print(an[0])
-
-    all_cols = list(set(cont+inter+fixed))
-    gd = patients[all_cols]
-
-    an = lstm_model(48,gd)
-    a.append(an[0])
-    #b = mean_confidence_interval(an[1])
-    #c = mean_confidence_interval(an[2])
-
-
-
-
-
-# In[ ]:
-
-print('Fixed')
-print(mean_confidence_interval(list(itertools.chain(*f_a))))
-print('Inter')
-print(mean_confidence_interval(list(itertools.chain(*i_a))))
-print('Cont')
-print(mean_confidence_interval(list(itertools.chain(*c_a))))
-print('Cont+inter')
-print(mean_confidence_interval(list(itertools.chain(*ci_a))))
-print('Fixed+Inter')
-print(mean_confidence_interval(list(itertools.chain(*fi_a))))
-print('Cont+Fixed')
-print(mean_confidence_interval(list(itertools.chain(*cf_a))))
-print('All')
-print(mean_confidence_interval(list(itertools.chain(*a))))
-
-
+except Exception as e:
+    print(e, "main code error")
 
 
 
